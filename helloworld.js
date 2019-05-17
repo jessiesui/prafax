7bf15d2d3e819444d1d9861286627a81ba41f85f
jessiesui <47791914+jessiesui@users.noreply.github.com>
2019-05-17 18:40:35 -0400

    wrapped in function

--- helloworld.js
+++ helloworld.js
@@ -1,11 +1,13 @@
-const accountSid = 'AC1c575398f328b2a4eb564728a296be08';
-const authToken = '536cf42fce7053baea97bd52fc3d20c1';
-const client = require('twilio')(accountSid, authToken);
+function sendText() {
+  const accountSid = 'AC1c575398f328b2a4eb564728a296be08';
+  const authToken = '536cf42fce7053baea97bd52fc3d20c1';
+  const client = require('twilio')(accountSid, authToken);
 
-client.messages
-  .create({
-     body: 'Your verification code is ' + (Math.floor(Math.random()  * (999 - 100) + 100)).toString(),
-     from: '+13212097129',
-     to: '+14076834035'
-   })
-  .then(message => console.log(message.sid));
+  client.messages
+    .create({
+       body: 'Your verification code is ' + (Math.floor(Math.random()  * (999 - 100) + 100)).toString(),
+       from: '+13212097129',
+       to: '+14076834035'
+     })
+    .then(message => console.log(message.sid));
+}
